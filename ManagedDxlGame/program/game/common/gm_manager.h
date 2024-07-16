1fee4f2e86b21141833e3f15ea1610549769fac1
SasamuraRitsuki <tetrapodmatcha@outlook.jp>
2024-07-16 12:36:53 +0900

    0716

--- ManagedDxlGame/program/game/common/gm_manager.h
+++ ManagedDxlGame/program/game/common/gm_manager.h
@@ -5,23 +5,29 @@ class SceneBase;
 
 class GameManager {
 public:
-    static GameManager* GetInstance(SceneBase* start_scene = nullptr);
+    //static GameManager* GetInstance(SceneBase* start_scene = nullptr);
+    static Shared<GameManager> GetInstance(Shared<SceneBase> start_scene = nullptr);
     static void Destroy();
 
-    void changeScene(SceneBase* next_scene, float trans_time = 0.5f);
+    //void changeScene(SceneBase* next_scene, float trans_time = 0.5f);
+    void changeScene(Shared<SceneBase> next_scene, float trans_time = 0.5f);
 
     void update(float delta_time);
 
-    inline SceneBase* getSceneInstance() { return now_scene_; }
+  // inline SceneBase* getSceneInstance() { return now_scene_; }
+    inline Shared<SceneBase> getSceneInstance() { return now_scene_; }
     inline bool getTransitionFlg() { return isTransitioning_; }
 
     int score_ = 0;
 
 private:
-    GameManager(SceneBase* start_scene);
+    //GameManager(SceneBase* start_scene);
+    GameManager(Shared<SceneBase> start_scene);
     float trans_time_ = 0.5f;
-    SceneBase* now_scene_ = nullptr;
-    SceneBase* next_scene_ = nullptr;
+    //SceneBase* now_scene_ = nullptr;
+    //SceneBase* next_scene_ = nullptr;
+    Shared<SceneBase> now_scene_ = nullptr;
+    Shared<SceneBase> next_scene_ = nullptr;
     int tansition_graph_hdl_ = 0;
 
     //SceneResult* result_ = nullptr;
